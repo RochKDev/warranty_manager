8841d82f74588c1a19ecbb14039de5ad57911cde
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-08-06 13:56:53 +0200

    feat(proof of purchase): add proof of purchase service interface

---
+++ backend-web/src/main/java/warranty/api/services/ProofOfPurchaseService.java
@@ -0,0 +1,15 @@
+package warranty.api.services;
+
+import warranty.api.model.Product;
+import warranty.api.model.ProofOfPurchase;
+import warranty.api.model.dto.ProofOfPurchaseId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProofOfPurchaseService {
+    ProofOfPurchase save(final ProofOfPurchase proofOfPurchase);
+    Optional<ProofOfPurchase> findOne(final ProofOfPurchaseId proofOfPurchaseId);
+    List<ProofOfPurchase> findAll(final Long userId);
+    void deleteById(final ProofOfPurchaseId proofOfPurchaseId);
+}
