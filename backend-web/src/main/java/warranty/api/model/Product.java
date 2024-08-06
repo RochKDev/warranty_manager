d6e0c5101e9481086c86478859ff7ba5b7acfea2
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-08-06 15:58:37 +0200

    feat(product): add builder to product

--- backend-web/src/main/java/warranty/api/model/Product.java
+++ backend-web/src/main/java/warranty/api/model/Product.java
@@ -3,6 +3,7 @@ package warranty.api.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Product {
 
     @Id
