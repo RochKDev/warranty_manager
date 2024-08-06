7f248b4df8e538267450eaa7aa95dd3bda805dbe
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-08-06 15:59:11 +0200

    feat(product service): save method with dto

--- backend-web/src/main/java/warranty/api/services/ProductService.java
+++ backend-web/src/main/java/warranty/api/services/ProductService.java
@@ -1,21 +1,22 @@
 package warranty.api.services;
 
 import warranty.api.model.Product;
+import warranty.api.model.dto.ProductDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
 
-    Product save(Product product);
+    Product save(final ProductDto productDto);
 
-    List<Product> findByShopNameAndReference(String shopName, String reference);
+    List<Product> findByShopNameAndReference(final String shopName, final String reference);
 
-    List<Product> findByUserId(Long userId);
+    List<Product> findByUserId(final Long userId);
 
-    Optional<Product> findOne(Long id);
+    Optional<Product> findOne(final Long id);
 
-    Product update(Long id, Product product);
+    Product update(final Long id, Product product);
 
-    void deleteById(Long id);
+    void deleteById(final Long id);
 }
