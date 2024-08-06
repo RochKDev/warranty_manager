76654ed9ad3d2a2a509a5e83bd68d2b224473946
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-08-06 14:54:02 +0200

    feat(custom exception): create proof of purchase custom exception

---
+++ backend-web/src/main/java/warranty/api/exception/ProofOfPurchaseNotFound.java
@@ -0,0 +1,8 @@
+package warranty.api.exception;
+
+public class ProofOfPurchaseNotFound extends RuntimeException{
+
+    public ProofOfPurchaseNotFound(String message){
+        super(message);
+    }
+}
