5b479aabe6367fd204cfa43cb58ce1f35cf87d9e
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-08-07 18:14:04 +0200

    feat(product controller): continue controller implementation
    continue crud methods

--- backend-web/src/main/java/warranty/api/controller/ProductController.java
+++ backend-web/src/main/java/warranty/api/controller/ProductController.java
@@ -7,6 +7,8 @@ import warranty.api.model.Product;
 import warranty.api.model.dto.ProductDto;
 import warranty.api.services.ProductService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/v1/products")
 public class ProductController {
@@ -19,6 +21,27 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Product> create(@RequestBody ProductDto productDto) {
         return new ResponseEntity<>(productService.save(productDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Product> getOne(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.findOne(id), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        return new ResponseEntity<>(productService.update(id, productDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
