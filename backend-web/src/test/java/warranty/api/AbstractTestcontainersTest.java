dfec1b0b3bdf1962f9da3b0176a06b7690ce2dc5
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-08-15 21:09:18 +0200

    test(test containers) : create abstract class for setting up a test container db

---
+++ backend-web/src/test/java/warranty/api/AbstractTestcontainersTest.java
@@ -0,0 +1,25 @@
+package warranty.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public abstract class AbstractTestcontainersTest {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mysqlContainer
+            = new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+
+    @Test
+    void canEstablishConnection() {
+        assertThat(mysqlContainer.isCreated()).isTrue();
+        assertThat(mysqlContainer.isRunning()).isTrue();
+    }
+}
