c0bd45da3111de5ff879562611b1362cacffabda
RochKDev <kowalski.rochpro@gmail.com>
2024-08-20 15:00:26 +0200

    feat(style navbar) : stylising the navbar.

--- frontend-web/src/components/Navbar.tsx
+++ frontend-web/src/components/Navbar.tsx
@@ -1,19 +1,47 @@
-import {Link} from 'react-router-dom';
+import {Menubar} from 'primereact/menubar';
+import {MenuItem} from 'primereact/menuitem';
+import {Button} from 'primereact/button';
+import {PrimeIcons} from 'primereact/api';
+
+import 'primeicons/primeicons.css';
+import {useState} from "react";
+
 
 export const Navbar = () => {
+    const [receipt, setReceipt] = useState<string>("Receipts");
+    const [product, setProduct] = useState<string>("Products");
+
+    const items: MenuItem[] = [
+        {
+            label: 'Warranty Manager',
+            icon: PrimeIcons.HOME,
+        },
+        {
+            label: 'Dashboard',
+            icon: 'pi pi-objects-column'
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope',
+            disabled: true,
+            visible : false
+        }
+    ];
+
+    const start = (
+        <div>
+            <i className="pi pi-bars"></i>
+        </div>
+    );
+    const end = (
+        <div className="flex align-items-center gap-2">
+            <Button label="Sign Up"/>
+        </div>
+    );
+
     return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+        <div className="w-full">
+            <Menubar className="w-full justify-center" model={items} start={start} end={end}/>
+        </div>
+    )
 }
--- frontend-web/src/main.tsx
+++ frontend-web/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router.tsx";
+import 'primereact/resources/themes/saga-blue/theme.css';  // or other theme
+import 'primereact/resources/primereact.min.css';          // core css
+import 'primeicons/primeicons.css';                        // icons
+import 'primeflex/primeflex.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
