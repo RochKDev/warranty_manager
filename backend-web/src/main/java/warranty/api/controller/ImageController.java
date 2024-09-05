040c1a53b78162279c5fa943149951cc8323d476
RochKDev <kowalski.rochpro@gmail.com>
2024-09-05 15:41:42 +0200

    fix(ImageController) : added automatic file extension detection.

--- backend-web/src/main/java/warranty/api/controller/ImageController.java
+++ backend-web/src/main/java/warranty/api/controller/ImageController.java
@@ -1,5 +1,7 @@
 package warranty.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import warranty.api.services.ImageService;
 
+import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "api/v1/images")
 public class ImageController {
@@ -21,15 +25,33 @@ public class ImageController {
 
     @PostMapping
     public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        byte[] fileBytes = null;
+        try {
+            fileBytes = file.getBytes();
+        } catch (IOException e) {
+            log.error("Error reading file: {}", originalFilename);
+            throw new RuntimeException(e);
+        }
+        String contentType = determineImageType(fileBytes);
+        log.info("Uploaded file type: {}", contentType);
         String uploadImage = imageService.uploadImage(file);
         return ResponseEntity.ok(uploadImage);
     }
 
+
     @GetMapping(path = "/{name}")
     public ResponseEntity<?> downloadImage(@PathVariable("name") String name) {
         byte[] downloadImage = imageService.downloadImage(name);
+        String contentType = determineImageType(downloadImage);
+        log.info("Content type: {}", contentType);
         return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.IMAGE_JPEG)
+                .contentType(MediaType.parseMediaType(contentType))
                 .body(downloadImage);
+    }
+
+    private String determineImageType(byte[] imageBytes) {
+        Tika tika = new Tika();
+        return tika.detect(imageBytes);
     }
 }
